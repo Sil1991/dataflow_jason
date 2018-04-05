2396c0d570b0ba2baeefdee2c3cf53c6a47752c0
Jason_Lai <jason_l_l@qq.com>
2018-04-06 03:22:40 +0800

    sdk enrichment

---
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/SdkDependencies.java
@@ -0,0 +1,13 @@
+package com.google.cloud.dataflow.sdk;
+
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.runners.direct.DirectRunner;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+
+class SdkDependencies {
+    private Pipeline p;
+    private BigQueryIO bigQueryIO;
+    private DirectRunner directRunner;
+    private DataflowRunner dataflowRunner;
+}
